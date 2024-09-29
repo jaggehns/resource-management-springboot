3dd2201dae6b07145e8e712fc6cd23d4feff0b45
Jaggehn <jaggehns@gmail.com>
2024-09-30 06:52:19 +0800

    feat: added exception handler

--- src/main/java/com/jaggehn/controller/ResourceController.java
+++ src/main/java/com/jaggehn/controller/ResourceController.java
@@ -4,20 +4,24 @@ import com.jaggehn.service.ResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
+
 @RestController
 @RequestMapping("/api/resource")
+@Validated
 public class ResourceController {
 
     @Autowired
     private ResourceService resourceService;
 
     @GetMapping("/access")
-    public ResponseEntity<String> accessResource(@RequestParam(defaultValue = "1") int priority) {
+    public ResponseEntity<String> accessResource(@RequestParam(defaultValue = "1") @Min(1) int priority) {
         resourceService.addRequest(priority);
         return ResponseEntity.status(HttpStatus.OK).body("Request added to queue with priority: " + priority);
     }
---
+++ src/main/java/com/jaggehn/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.jaggehn.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation Error: " + e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation Error: " + e.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + e.getMessage());
+    }
+}
